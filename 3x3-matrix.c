d141f1cd2830842d1c340aa2a55e69a68af707cb
Bishal Aryal <70318933+C15C01337@users.noreply.github.com>
2022-10-08 00:14:09 +0545

    Create 3x3-matrix.c

---
+++ 3x3-matrix.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+int a[3][3],i,j;
+for(i=0;i<3;i++)
+for(j=0;j<3;j++){
+printf("Enter Element at %d%d position",i+1,j+1);
+scanf("%d",&a[i][j]);
+
+}
+for(i=0;i<3;i++){
+for(j=0;j<3;j++){
+printf(" %d ",a[i][j]);
+}
+printf("\n");
+
+}
+return 0;
+
+}
